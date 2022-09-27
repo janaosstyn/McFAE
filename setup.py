f0bf05acb80132037b4c8b28714f6d230b554492
Ceder <ceder.dens@uantwerpen.be>
2022-09-27 11:56:06 +0200

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='McFAE',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     url='https://github.com/PigeonMark/McFAE',
     license='Apache 2.0',
